d6d4153c6329211e4bd467aace3829eba519ebb4
danteruizv8 <53718344+danteruizv8@users.noreply.github.com>
2020-05-25 23:27:29 -0300

    agrupa operaciones

--- octave/brillo_contraste.m
+++ octave/brillo_contraste.m
@@ -1,7 +1,6 @@
 function Q=brillo_contraste(I,B,C)
 I_d=double(I); % convierte los datos a tipo flotante
-I_bn=I_d(:,:,1)+I_d(:,:,2)+I_d(:,:,3); % suma el valor de R+G+B para cada elemento
-I_bn=I_bn/3; % divide para obtener el promedio
+I_bn=(I_d(:,:,1)+I_d(:,:,2)+I_d(:,:,3))/3; % promedio de R+G+B para cada elemento
 I_max=max(I_bn(:)); % obtiene el valor maximo de la matriz
 I_min=min(I_bn(:)); % obtiene el minimo
 I_d=((I_d-I_min)/(I_max-I_min)*(C+1)+(1-C))*128; % funcion para ajustar contraste
