51a27695e8cd8e82a4bd6969bb0ac84aa0d0cc58
gustavo <dariogustavoalbarran@gmail.com>
2020-08-09 16:17:26 -0300

    Ej 4.2 corregido

--- TP4/e4_2.m
+++ TP4/e4_2.m
@@ -55,18 +55,18 @@ Rp = 0.5; % [dB] (max) mag2db(db2mag(0.5)-1) -> ans = -24.546  [dB]
 Sba = 50; % [dB] (min) - 50 [dB] es la condicion mas exigente
 % minima cantidad de taps
 
-% se elige la ventana Blackman - Amplitud el pico mayor: -74 dB
+% se elige la ventana hamming - Amplitud el pico mayor: -53 dB
 
 fpass = 2 * fpass_hz / fs; %'^\_se convierte a frecuencias digitales
 fstop = 2 * fstop_hz / fs; % _/
 ft = (fstop + fpass) / 2; % frecuencia de corte ideal
 B = fstop - fpass; % ancho de banda de transicion
-M = ceil(12/B); % cantidad de taps para ventana hamming -> Ancho de lobulo principal = 12/M
-% ceil (x) redondea al entero mayor o igual, mas cercano a 'x' 
+M = ceil(8/B); % cantidad de taps para ventana hamming -> Ancho de lobulo principal = 8/M
+% ceil (x) redondea al entero mayor o igual, mas cercano a 'x'
 n = (0 : (M-1)); % elementos del vector h_sinc
 
 h_sinc = ft * sinc (ft * (n - ((M-1)/2))); % funcion sinc del filtro
-h = h_sinc .* (blackman (M))';
+h = h_sinc .* (hamming (M))';
 
 H = fft(h,1000);  % calcula la FFT con 1000 puntos de frecuencia, completa con ceros
 Hdb = mag2db( abs( H(1:500) ) ); % convierte a dB el valor absoluto de las muestras de f positiva
@@ -77,7 +77,7 @@ fd = (0 : fs/1000 : ((fs/2)-(fs/1000)) ) / 1000; % vector con los valores de fre
 
 figure(2, 'name','Guia 4 ejercicio 2.2','Units','normalized','Position',[0 0 .5 1]); % mitad de pantalla
 plot (fd, Hdb, 'linewidth', 3); % grafica la respuesta en frecuencia
-axis([0 20 -200 10]); % limites de los ejes
+axis([0 20 -120 5]); % limites de los ejes
 xlabel ('Frecuencia [kHz]'); % etiqueta eje X
 ylabel ('Respuesta al impulso |H(f)| [dB]');  % etiqueta eje y
 title ('Filtro pasa-bajos con f_{pass}=3kHz  ,  f_{stop} = 4kHz  ,  f_s = 44.1kHz  ,  ventana Blackman'); % titulo
