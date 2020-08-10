d130c8019109dddcb48adc5ebc0558298dfb708d
gustavo <dariogustavoalbarran@gmail.com>
2020-08-09 23:08:13 -0300

    Modificacion del ej 4.4 Considerando B como el ancho de banda de transicion. Antes lo tomaba como ancho de banda de paso.

--- TP4/e4_4.m
+++ TP4/e4_4.m
@@ -1,4 +1,4 @@
-% <------- Guia 4 - Ejercicio 3 ----------->
+% <------- Guia 4 - Ejercicio 4 ----------->
 
 clc; % borra la consola
 clear all; % borra todas las variables
@@ -14,31 +14,31 @@ B = 0.01; % ancho de la banda de paso
 
 % se elige la ventana blackman - Amplitud del pico mayor: -74 dB
 
-% ^                       B                           
-% |H(f)               |<----->|                      
-% |                   ---------                      
-% |                  /         \                     
-% |                 /           \                    
-% |                /|           |\                   
-% |               / |           | \                  
-% |---------------  |           |  ---------------   
-% |             |   |           |   |                
-% |-------------+---+-----------+---+--------------->
-%              fc1  |           |  fc2             f 
-%                 ft_pa   |    ft_pb                 
+% ^                B            B             
+% |H(f)         |<--->|      |<--->|                      
+% |             |    .--------.    |                 
+% |             |   /          \   |                 
+% |             |  /            \  |                 
+% |             | /|            |\ |                 
+% |             |/ |            | \|                 
+% |-------------'  |            |  '--------------   
+% |             |  |            |  |                
+% |-------------+--+------------+--+--------------->
+%             fc1  |            |  fc2             f 
+%                 ft_pa   |   ft_pb                 
 %    Fpa                  |                   Fpb    
 %  <----------------------+------------------------> 
 
-ft_pa = (3* fc1 + fc2 - B)/4; % frec transicion del pasa altos
-ft_pb = (3* fc2 + fc1 + B)/4; % frec transicion del pasa bajos
+ft_pa = fc1 + B/2; % frec transicion del pasa altos
+ft_pb = fc2 - B/2; % frec transicion del pasa bajos
 
-Bt = (ft_pa + fc1)/2; % ancho de banda de transicion
+%Bt = (ft_pa + fc1)/2; % ancho de banda de transicion
 
 n_fft = 4000; % muestras para el fft
 pn_fft = n_fft/2; % muestras para el fft
 fd = (0 : 1/pn_fft : 1-(1/pn_fft) ); % vector con los valores de frecuencia digital fd = 2f / fs
 
-M = ceil(12/Bt); % cantidad de taps para ventana blackman -> Ancho de lobulo principal = 12/M
+M = ceil(12/B); % cantidad de taps para ventana blackman -> Ancho de lobulo principal = 12/M
 M = 2*floor(M/2)+1; % convierte M al impar mayor o igual. floor(x) redondea al entero <= x
 % asegura una cantidad impar de coeficientes para que el retardo D resulte en un número entero
 n = (0 : (M-1)); % elementos de los vectores h_sinc
@@ -63,13 +63,13 @@ Hdb_pbanda = mag2db( abs( H_pbanda(1:pn_fft) ) ); % convierte a dB el valor abso
 %H_pb = fft(h_pb,n_fft);  % calcula la FFT con n_fft puntos de frecuencia, completa con ceros
 %Hdb_pb = mag2db( abs( H_pb(1:pn_fft) ) ); % convierte a dB el valor absoluto de las muestras de f positiva
 
-figure(1, 'name','Guia 4 ejercicio 3.1 y 3.2','Units','normalized','Position',[0 0 1 1]); % pantalla completa
+figure(1, 'name','Guia 4 ejercicio 4','Units','normalized','Position',[0 0 1 1]); % pantalla completa
 %subplot(1,2,1);  % subplot (filas, columnas, indice)
 %plot (fd, Hdb_pb, 'linewidth', 1.5); % grafica la respuesta en frecuencia
 %hold on;
 %plot (fd, Hdb_pa, 'linewidth', 1.5); % grafica la respuesta en frecuencia
 plot (fd, Hdb_pbanda, 'linewidth', 1.5); % grafica la respuesta en frecuencia
-%axis([0 .5 -150 10]); % limites de los ejes
+axis([0 .3 -150 10]); % limites de los ejes
 grid on;
 grid minor on;
 xlabel ('Frecuencia digital ( f_d = 2·f / f_s )'); % etiqueta eje X
---
+++ TP4/e4_5.m
@@ -0,0 +1,7 @@
+% <------- Guia 4 - Ejercicio 5 ----------->
+
+clc; % borra la consola
+clear all; % borra todas las variables
+close all; % cierra las ventanas de imagen
+pkg load control; % paquete con 'mag2db'
+pkg load signal; % ventanas
