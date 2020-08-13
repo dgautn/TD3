43587c11fe3614b69518deb42f8cfa02e4bd4be8
gustavo <dariogustavoalbarran@gmail.com>
2020-08-12 21:32:26 -0300

    ej 4.7 completo

--- TP4/e4_7.m
+++ TP4/e4_7.m
@@ -26,22 +26,26 @@ B = [0.0528556 0.0017905 0.0017905 0.0528556];
 A = [1.00000 -2.12984 1.78256 -0.54343];
 
 ts = 1/fs; % [s] periodo de muestreo
+frec = (0 : 1 : (fs/2)-1 ); % vector con los valores de frecuencia (el primer elemento es 0Hz
 
 tonos = (sin(2*pi*f1* [0:ts:d-ts] )) + (sin(2*pi*f2* [0:ts:d-ts] )); % suma de 2 tonos senoidales
 y = filtro_iir (B, A, tonos); % aplica el filtro IIR del punto 1
 Y = fft (y, fs); % calcula la FFT con cantidad de puntos de frecuencia igual a la frec de muestreo
-Ypos = abs( Y(1:fs/2) ); % guarda el valor absoluto de las muestras de f positiva
+Ypos = abs( Y(1:(fs/2)) ); % guarda el valor absoluto de las muestras de f positiva
 %Ydb = mag2db( abs( Y(1:fs/2) ) ); % convierte a dB el valor absoluto de las muestras de f positiva
 
-figure(1, 'name','Guia 4 ejercicio 7','Units','normalized','Position',[0 0 1 1]); % pantalla completa
-subplot(2,2,1);  % subplot (filas, columnas, indice)
-plot (Ypos, 'linewidth', 1.5); % grafica del espectro
-%axis([0 2000]); % limites de los ejes
+figure(1, 'name','Guia 4 ejercicio 7','Units','normalized','Position',[0 0 1 1]); % mitad de pantalla
+subplot(2,1,1);  % subplot (filas, columnas, indice)
+plot (frec, Ypos, 'c', 'linewidth', 3.5); % grafica del espectro
+axis([0 127]); % limites de los ejes
 grid on;
 grid minor on;
 xlabel ('Frecuencia [Hz]'); % etiqueta eje X
 ylabel ('Amplitud');  % etiqueta eje y
-title (''); % titulo
+title ('Señal bitonal de 50Hz y 100Hz filtrada'); % titulo
+
+text (50, Ypos(51), [num2str(Ypos(51)), "\n  50Hz"], "horizontalalignment", "left");
+text (100, Ypos(101), [num2str(Ypos(101)), "\n  100Hz"], "horizontalalignment", "left");
 
 %%%%%%%%%%%
 % punto 3 %
@@ -53,5 +57,32 @@ Y2pos = abs (Y2 (1:fs/2)); % guarda el valor absoluto de las muestras de f posit
 %Y2db = mag2db (abs (Y2 (1:fs/2))); % convierte a dB el valor absoluto de las muestras de f positiva
 
 hold on;
-plot (Y2pos, 'linewidth', 1.5); % grafica del espectro
-%sound (tonos, fs);
+plot (frec, Y2pos,  'r--', 'linewidth', 1); % grafica del espectro
+legend({'IIR implementado', 'funcion filter'}, 'location', 'northwest');
+legend('boxoff');
+hold off;
+
+%%%%%%%%%%%
+% punto 4 %
+%%%%%%%%%%%
+
+[H, W] = freqz (B, A); % devuelve la respuesta en frec de un IIR - W vector de frecuencias
+W_hz = W/(2*pi)*fs; % W esta en frec angular - pasamos a Hz
+H_db = mag2db(abs(H)); % convierte a dB
+subplot(2,1,2);  % subplot (filas, columnas, indice)
+plot (W_hz, H_db, 'linewidth', 1.5);
+axis([0 127]); % limites de los ejes
+grid on;
+grid minor on;
+xlabel ('Frecuencia [Hz]'); % etiqueta eje X
+ylabel ('Magnitud [dB]');  % etiqueta eje y
+title ('Respuesta en frecuencia del filtro'); % titulo
+
+hold on;
+plot(50, H_db(200),'rx', 'linewidth', 3)
+plot(100, H_db(400),'rx', 'linewidth', 3)
+text (50, H_db(200), [num2str(H_db(200)), " dB\n  50Hz"], "verticalalignment", "top");
+text (100, H_db(400), [num2str(H_db(400)), " dB\n  100Hz"], "verticalalignment", "top");
+
+% 50 * length (H_db) / (fs/2) -> ans =  200  % elemento del vector para cada frec 
+% 100 * length (H_db) / (fs/2) -> ans =  400 % 
--- TP4/filtro_iir.m
+++ TP4/filtro_iir.m
@@ -6,23 +6,23 @@ function y = filtro_iir (B, A, senial)
 % salida -> salida filtrada
 % B, A -> coeficientes
 
-N = length(B); % o length(A) ??? Orden del filtro
-M = length(senial); % longitud de la señal de entrada
+N = length(B); % Orden del filtro
+M = length(A); % si es mayor Orden del filtro
+L = length(senial); % longitud de la señal de entrada
 
-y = zeros(1, N+M-1); % vector resultado
+y = zeros(1, N+L-1); % vector resultado
 r_des_ent = zeros(1, N); % registro de desplazamiento para las muestras de entrada
 r_des_sal = zeros(1, N); % registro de desplazamiento para las muestras de salida puede ser de N-1 ??
 
 senial_ext = [senial, zeros(1, N-1)]; % vector de señal extendido
-for n = 1:M+N-1
+for n = 1:L+N-1
   r_des_ent = [senial_ext(n), r_des_ent(1:N-1)]; % llena el registro de desplazamiento con las ultimas N muestras de x
   for i = 1:N
-    y(n) = y(n) + ( B(i) * r_des_ent(i) ); % algoritmo del filtro IIR suma productos de b[i]
+    y(n) = y(n) + ( B(i)/ A(1) * r_des_ent(i) ); % algoritmo del filtro IIR suma productos de b[i]
   end
-  for i = 2:N % empieza del segundo elemento
-    y(n) = y(n) - ( A(i) * r_des_sal(i-1) ); % algoritmo del filtro IIR resta productos de a[i]
+% se divide por A(1) -> segun el algoritmo y[n] = 1/ a[0] (......)
+  for i = 2:M % comienza del segundo elemento
+    y(n) = y(n) - ( A(i)/ A(1) * r_des_sal(i-1) ); % algoritmo del filtro IIR resta productos de a[i]
   end
-  r_des_sal = [y(n), r_des_sal(1:N-1)]; %llena el registro de desplazamiento con las ultimas N salidas
+  r_des_sal = [y(n), r_des_sal(1:N-1)]; % llena el registro de desplazamiento con las ultimas N salidas
 end
-
-y = y / A(1); % segun el algoritmo y[n] = 1/ a[0] (......)
