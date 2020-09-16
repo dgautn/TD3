a31ed567cf52da286425f8f49e775fa3418341e3
gustavo <dariogustavoalbarran@gmail.com>
2020-09-16 13:00:53 -0300

    ej 4

--- TP7/e7_4.m
+++ TP7/e7_4.m
@@ -5,8 +5,29 @@ clear all; % borra todas las variables
 close all; % cierra las ventanas de imagen
 
 [h, fs_h]=audioread('s1_r1_b_cd.wav');
-[x, fs_x]=audioread('_');
+[x, fs_x]=audioread('Little_Wing.wav');
 
-M = length(h);
-N = 4*M;
-L = N - (M - 1);
+M = length(h); % Longitud de la respuesta al impulso del filtro
+N = 4*M; % Cantidad de muestras de la FFT
+L = N - (M - 1); % Cantidad de muestras procesadas por bloque
+
+x_l = length(x);
+x = [x ; zeros(L*ceil(x_l/L) - x_l, 2)]; % se agregan ceros para tenr una cantidad entera de L
+x = [zeros((M-1), 2) ; x]; % se gregan ceros al comienzo, que se eliminan en el filtrado
+H = fft(h, N); % Respuesta al impulso del filtro
+
+y = [0, 0];
+l = 1;
+while l <= x_l
+%  l
+%  l + N - 1
+  muestra = x(l : l + N - 1, 2);
+  Muestra = fft(muestra, N);
+  Muestra = Muestra .* H;
+  muestra = ifft(Muestra);
+  y = [y ; muestra(M:end,2)];
+  l = l + N -(M-1);
+endwhile
+
+y = y(2:end - (L*ceil(x_l/L) - x_l) ,2); % se quitan los ceros agregados al final
+sound(y, fs_x);
